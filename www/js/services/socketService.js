eede09ad3ddc7db1bb41cf876a3673264344d78f
mikiLukasik <szarhazi@gmail.com>
2016-07-03 16:30:07 +0100

    socket is now running in cordova, cordova plugin replaced

--- www/js/services/socketService.js
+++ www/js/services/socketService.js
@@ -8,12 +8,12 @@ services.factory('socketService', function($rootScope, $timeout) {
     
     if ("WebSocket" in window) {
       console.log('Websockets found in window.')
-      
+      console.log(WebSocket)
       
       socket.connect = function(){
         
         // Let us open a web socket
-        socket.ws = new WebSocket('ws://miki.ddns.net/sockets/');//,["WebSocket"]);
+        socket.ws = new WebSocket('ws://miki.ddns.net:3000/sockets/');//,["WebSocket"]);
                 // socket.ws = new WebSocket('ws://localhost:3000/sockets/');//,["WebSocket"]);
 
         // var socket_host = 'http://localhost:3000'//window.location.origin + ':8080';
@@ -24,9 +24,7 @@ services.factory('socketService', function($rootScope, $timeout) {
         //     upgrade: false,
         //     cookie: false
         // };
-        socket.ws.onerror = function (err){
-          throw new Error(err);
-        }
+        
         //Create socket
         // var socketIo = io(socket_host, socket_args);
 
@@ -37,9 +35,39 @@ services.factory('socketService', function($rootScope, $timeout) {
         // //var socketIo = io.connect('http://localhost:3000', {path: '/socket.io/'});
         // //var socketIo = io.connect('http://localhost:3000', {path: '/sockets/'});
         // socketIo.emit({a:1})  
+        
+        socket.ws.onmessage = function (evt) {
+          
+          console.log('onmessage',evt)
+          
+          //wsOnmessageFunc(evt, $rootScope, $scope, ws, indexGlobals)
+        }
+        
+        socket.ws.onerror = function (err, e2){
+          console.log('Error:',err, e2)
+          throw new Error(err);
+          
+        }
+
+       socket.ws.onclose = function () {
+          // websocket is closed.
+
+          socket.socketOn = false
+
+          socketID = undefined
+
+          if ($rootScope.settingsTab.online) {
+            console.log("socketConnection is closed, retry in 2s..");
+             $timeout(function () {
+                if (!socket.socketOn && $rootScope.settingsTab.online) socket.connect()
+              }, 2000)
+          }
+         
+        };
+        
         socket.ws.onopen = function (openEvent) {
           // Web Socket is connected, send data using send()
-          
+          //this.send({a:1})
           console.log('WebSocket opened, event.target:', openEvent.target)
           
           socket.openedSocket = openEvent.target;
@@ -64,14 +92,14 @@ services.factory('socketService', function($rootScope, $timeout) {
               var cb = function () {}
             }
 
-            socket.openedSocket.send(JSON.stringify(sendThis), cb);
+            socket.ws.send(JSON.stringify(sendThis))//, cb);
 
           }
 
           console.log("socket connected, let's say hello...")
 
-          console.log(socket.ws)
-         // socket.ws.send = socket.ws.__proto__.send
+          //console.log(socket.ws)
+         // socket.ws.send = socket.ws.__proto__.sendconsole.log(err)
                     
           $timeout(function(){
            socket.send('Hello', {
@@ -85,28 +113,7 @@ services.factory('socketService', function($rootScope, $timeout) {
 
         };
 
-        socket.ws.onmessage = function (evt) {
-          
-          console.log('onmessage',evt)
-          
-          //wsOnmessageFunc(evt, $rootScope, $scope, ws, indexGlobals)
-        }
-
-       socket.ws.onclose = function () {
-          // websocket is closed.
-
-          socket.socketOn = false
-
-          socketID = undefined
-
-          if ($rootScope.settingsTab.online) {
-            console.log("socketConnection is closed, retry in 2s..");
-             $timeout(function () {
-                if (!socket.socketOn && $rootScope.settingsTab.online) socket.connect()
-              }, 2000)
-          }
-         
-        };
+        
           
         
         
