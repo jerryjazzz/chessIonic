5b33b45c16a39df6f3a5699a25ee2339097ce068
mikiLukasik <miklosbacsi@gmail.com>
2016-10-11 03:35:55 +0100

    .

--- server/public/js/server/routerFuncs.js
+++ server/public/js/server/routerFuncs.js
@@ -169,7 +169,7 @@ var initRouter=function(router,app){
         }
         sendDot()
 
-        exec("docker run -it --rm --privileged -v /dev/bus/usb:/dev/bus/usb -v $PWD:/src cordova cordova build", function (error, stdout, stderr) {
+        exec("cd .. && docker run -it --rm --privileged -v /dev/bus/usb:/dev/bus/usb -v $PWD:/src cordova cordova build", function (error, stdout, stderr) {
             running = false
             sys.print('stdout: ' + stdout);
             sys.print('stderr: ' + stderr);
