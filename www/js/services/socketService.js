59c09324ee82deab9bceef1a5304f131fa716199
mikiLukasik <miklosbacsi@gmail.com>
2016-10-12 02:24:24 +0100

    .sapce

--- www/js/services/socketService.js
+++ www/js/services/socketService.js
@@ -1,9 +1,6 @@
 services.factory('socketService', function($rootScope, $timeout, $q, speedTestService) {
   // console.log(document.location)
-  var serverAddresses = [
-    'ws://miki.ddns.net/sockets/',
-    'ws://192.168.1.90/sockets/',
-  ]
+  var serverAddresses = ['ws://miki.space/sockets/']
 
   if (document.location) serverAddresses.unshift('ws://' + document.location.host + '/sockets/')
 
